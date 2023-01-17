e3ce9a31d17ffb8a2ba5afdfe9908cd99df57141
rashed98bt <rashedbatayneh@gmail.com>
2023-01-17 14:12:56 +0300

    done

---
+++ matches-core/src/main/java/com/mycompany/matches/core/MatchesCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.matches.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchesCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchesCoreApplication.class, args);
+    }
+
+}
